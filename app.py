108bdc8c31324666bb928eb9c3e337116f27609e
Jyoshna1429 <jyoshnachoudhury07@gmail.com>
2026-07-28 23:19:06 +0530

    Added about page

--- app.py
+++ app.py
@@ -156,6 +156,10 @@ def chat():
         return redirect(url_for("home"))
     return render_template("chat.html", **context)
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
